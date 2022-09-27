a904331e966868bc034f969ff3b51e0a0250af29
Erik <78443570+thekhachik@users.noreply.github.com>
2022-09-27 21:20:14 +0300

    Initial commit

--- app/src/main/java/Activities/TutorialsFragment.java
+++ app/src/main/java/Activities/TutorialsFragment.java
@@ -1,16 +1,25 @@
 package Activities;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.karenovich.goenglish.databinding.FragmentTutorialsBinding;
 
+import ViewModel.TutorialsViewModel;
+
 
 public class TutorialsFragment extends Fragment {
 
     FragmentTutorialsBinding binding;
+    TutorialsViewModel model;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,5 +31,17 @@ public class TutorialsFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentTutorialsBinding.inflate(inflater, container, false);
         return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        model = new ViewModelProvider(this).get(TutorialsViewModel.class);
+        binding.tutorialRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.tutorialRecycler.setHasFixedSize(true);
+
     }
 }
+
+
+
---
+++ app/src/main/java/Adapters/TutorialsAdapter.java
@@ -0,0 +1,52 @@
+package Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.karenovich.goenglish.databinding.ItemListBinding;
+
+import java.util.List;
+
+import Model.Tutorials;
+
+public class TutorialsAdapter extends RecyclerView.Adapter<TutorialsAdapter.MyViewHolder>{
+
+    private List<Tutorials> mTutorials;
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemListBinding binding = ItemListBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new MyViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Tutorials tutorials = mTutorials.get(position);
+        holder.bindView(tutorials);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTutorials.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        ItemListBinding binding;
+
+        public MyViewHolder(ItemListBinding binding){
+            super(binding.getRoot());
+            this.binding = binding;
+
+
+        }
+        public void bindView(Tutorials tutorials){
+            binding.nameTxt.setText(tutorials.getTheme());
+        }
+    }
+}
